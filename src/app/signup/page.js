7987647e680e10684ff095a8897cf9a62e90e4e1
sarvjit07 <sarvjitlala@gmail.com>
2023-12-20 22:01:21 +0530

    Update Sign-up Page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -3,7 +3,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { auth } from '../firebase';
-
+import Link from 'next/link';
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -12,10 +12,10 @@ export default function Signup() {
   const signup = () => {
     createUserWithEmailAndPassword(auth, email, password);
   };
-  
+
   return (
     <>
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
@@ -84,13 +84,15 @@ export default function Signup() {
             </div>
 
             <div>
-              <button
-                disabled={(!email || !password || !passwordAgain) || (password !== passwordAgain)}
-                onClick={() => signup()}
-                className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Sign Up
-              </button>
+              <Link href='giflist'>
+                <button
+                  disabled={(!email || !password || !passwordAgain) || (password !== passwordAgain)}
+                  onClick={() => signup()}
+                  className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Sign Up
+                </button>
+              </Link>
             </div>
           </div>
         </div>
